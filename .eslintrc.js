777e09924d990ba8741740d074a978948eeee634
r0b1337 <master@r0b.me>
2021-06-09 01:08:11 +0200

    improv(linting): add @typescript-eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   env: {
     node: true,
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
