ae554a1f107c3d47b611bc5ccad2f736570a0281
r0b1337 <master@r0b.me>
2021-06-10 00:45:11 +0200

    feat(components): :sparkles: Card

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="card bg-white text-2xl w-auto max-w-lg rounded-3xl text-left shadow-2xl p-10 font-bold"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
