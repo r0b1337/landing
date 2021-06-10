fa4b4cf41a3a127f4642fb61b8d1de7a560370e2
r0b1337 <master@r0b.me>
2021-06-11 00:56:13 +0200

    feat(app): :sparkles: intro Section

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,73 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <Section
+      class="bg-gradient-to-r from-yellow-300 bg-red-300"
+    >
+      <div class="left flex flex-col">
+        <span class="text-8xl lg:text-9xl font-bold">r0b1337</span>
+        <span class="text-3xl">Full Stack JS Developer</span>
+      </div>
+
+      <Card>
+        <img
+          class="h-40"
+          src="/javascript.svg"
+          alt="Javascript logo"
+        >
+      </Card>
+
+      <img
+        class="absolute bottom-5 h-10 animate-pulse cursor-pointer"
+        src="/scroll.svg"
+        alt="Scroll"
+        @click="scrollTo('neocity')"
+      >
+    </Section>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import ScrollReveal from 'scrollreveal';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
+  mounted() {
+    ScrollReveal({
+      interval: 50,
+      delay: 100,
+      easing: 'ease-out',
+    }).reveal('span', {
+      opacity: 0,
+      duration: 500,
+      distance: '10px',
+    });
+
+    ScrollReveal().reveal('.card', {
+      opacity: 0,
+      rotate: { z: 100 },
+      scale: 0,
+      easing: 'cubic-bezier(.75,-0.5,0,1.45)',
+    });
+  },
+  methods: {
+    scrollTo: function (hash: string) {
+      location.hash = hash;
+    },
   },
 });
 </script>
 
 <style>
+html {
+  scroll-behavior: smooth;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
