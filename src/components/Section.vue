30d891acf025ac11ac866e5f5e098f14685a6dcf
r0b1337 <master@r0b.me>
2021-06-11 00:30:02 +0200

    fix(Section): :ambulance: no background in production
    background classes from tailwind are purged in production because they are not detected because of template bindings

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,26 +1,7 @@
 <template>
   <div
-    :class="`from-${startColor}-300 to-${endColor}-300`"
-    class="w-screen bg-gradient-to-r text-white h-screen tracking-tighter flex flex-col md:flex-row justify-evenly items-center relative"
+    class="w-screen bg-gradient-to-r text-white h-screen tracking-tighter flex flex-col lg:flex-row justify-evenly items-center relative"
   >
     <slot />
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  props: {
-    startColor: {
-      type: String,
-      default: 'yellow',
-    },
-    endColor: {
-      type: String,
-      default: 'red',
-    },
-  },
-});
-
-</script>
