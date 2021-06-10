558e98f0b56d0b3257a67e2190412c7df7df436c
r0b1337 <master@r0b.me>
2021-06-11 01:19:50 +0200

    fix(Section): :bug: add gap

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-screen bg-gradient-to-r text-white h-screen tracking-tighter flex flex-col lg:flex-row justify-evenly items-center relative"
+    class="w-screen bg-gradient-to-r text-white h-screen tracking-tighter flex flex-col gap-10 lg:flex-row justify-evenly items-center relative"
   >
     <slot />
   </div>
