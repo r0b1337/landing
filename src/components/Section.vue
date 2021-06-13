be95dd14b26731b8f23e31e26cfe78f95567169b
r0b1337 <master@r0b.me>
2021-06-13 16:08:35 +0200

    fix(components): :bug: Section's height

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="section w-screen bg-gradient-to-r text-white h-screen tracking-tighter flex flex-col gap-10 lg:flex-row justify-evenly items-center relative"
+    class="section w-screen bg-gradient-to-r text-white min-h-screen h-auto p-20 tracking-tighter flex flex-col gap-10 lg:flex-row justify-evenly items-center relative"
   >
     <slot />
   </div>
