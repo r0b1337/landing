9860e85e28c1f39b9c990ed3911a8c12e97ab2b0
r0b1337 <master@r0b.me>
2021-06-21 21:35:25 +0200

    feat(App): :fire: decrease neocity lorem length

--- src/App.vue
+++ src/App.vue
@@ -37,23 +37,8 @@
         <span
           class="text-reveal text-left text-1xl max-w-xl mt-10 mb-2"
         >
-          Et ea commodo proident non nisi labore ipsum nisi veniam fugiat id. Velit sunt laboris exercitation amet laborum incididunt elit laboris elit in minim nulla Lorem. Esse in nulla amet deserunt ex dolor velit amet labore amet in id. Ea nulla qui ut ut nulla nostrud. Commodo esse labore reprehenderit ex nisi sunt nostrud commodo consequat.
-
-          Ipsum laborum laborum ipsum minim adipisicing amet. Exercitation aliqua quis id quis tempor. Dolor amet do enim officia anim. Quis veniam aute excepteur elit.
-
-          Esse sit cupidatat duis nisi excepteur exercitation officia eiusmod minim quis fugiat ipsum. Fugiat dolore culpa quis velit aliqua deserunt magna aliquip qui labore ullamco laboris deserunt voluptate. Lorem adipisicing do aliqua id ullamco. Pariatur id sint qui in consectetur id occaecat qui ad duis deserunt ipsum laboris.
-
-          Nisi reprehenderit minim reprehenderit magna id laboris nisi commodo sunt ex deserunt qui. Exercitation veniam aliqua velit Lorem est et esse sit mollit commodo irure culpa exercitation magna. Elit officia qui laboris voluptate aliquip tempor est. Consequat commodo est veniam Lorem cillum adipisicing veniam minim pariatur est tempor nostrud veniam minim.
-
-          Reprehenderit deserunt qui commodo reprehenderit magna ex duis. Cupidatat sit pariatur veniam sunt ipsum minim aliqua occaecat esse veniam. Sunt Lorem sint est aliqua voluptate laborum amet. Id eiusmod cillum anim mollit culpa excepteur nisi sit. Duis velit et eiusmod sit nostrud ut. Est exercitation incididunt non est non. Sint consequat ullamco consectetur voluptate laborum ipsum tempor do sint.
-
-          Consectetur Lorem veniam consequat anim voluptate exercitation eiusmod reprehenderit irure Lorem ut aliqua aliquip id. Dolore consequat velit nulla laboris eu velit excepteur laboris quis ad eiusmod occaecat dolore. Consequat ut amet voluptate amet.
-
-          Dolor magna voluptate pariatur veniam deserunt proident deserunt enim eiusmod sit deserunt. Culpa mollit qui deserunt ex culpa qui fugiat est cillum mollit. Labore culpa qui labore nisi aliquip. Nostrud eiusmod dolor sint deserunt officia do id ad esse irure enim. Dolore voluptate officia do do laboris reprehenderit id ex fugiat aliqua magna do.
-
-          Laboris amet enim excepteur culpa irure aliquip sint eu mollit. In minim consequat sunt ad reprehenderit ex. Voluptate quis ut occaecat commodo sunt.
-
-          Fugiat commodo dolor irure occaecat. Est duis reprehenderit aliquip veniam sint adipisicing id ea ex do qui. Consectetur id sit enim consectetur non commodo quis nostrud veniam pariatur esse fugiat pariatur deserunt. Duis exercitation esse commodo excepteur id sunt tempor qui cillum non. Id Lorem et esse ut incididunt dolore enim cillum. Aute est enim tempor culpa do.
+          Ea dolore veniam incididunt ullamco in officia minim labore irure in anim Lorem. Excepteur nisi pariatur sit amet elit mollit dolor. Sit cupidatat irure mollit cillum deserunt.
+          Amet ad dolor dolore excepteur sint dolor nisi pariatur reprehenderit sit veniam laborum nostrud. Elit ullamco ea elit consectetur. Id duis quis consectetur in voluptate. Ipsum sit reprehenderit labore ad minim nisi dolor anim fugiat elit laboris est nisi laborum. Officia nostrud fugiat irure ullamco ut duis. Est consequat culpa enim officia amet reprehenderit do tempor. Quis eu duis commodo dolore et dolore amet amet magna labore qui.
         </span>
       </div>
 
